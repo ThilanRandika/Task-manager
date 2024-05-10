0650d1087158f801d18ba9c1f7c26e250cd165d4
Thilan Randika <128400496+ThilanRandika@users.noreply.github.com>
2024-05-10 20:48:19 +0530

    Add basic structure of Home page and make mainActivity as a splash screen

--- app/src/main/java/com/example/task_manager/MainActivity.kt
+++ app/src/main/java/com/example/task_manager/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.task_manager
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val handler = Handler(Looper.getMainLooper())
+    private val delayInMillis: Long = 3000 // 3 seconds
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Post a delayed action to navigate to the Home screen after 3 seconds
+        handler.postDelayed({
+            startActivity(Intent(this@MainActivity, HomeActivity::class.java))
+            finish() // Optionally, finish the current activity to prevent going back
+        }, delayInMillis)
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Remove any pending delayed actions to avoid memory leaks
+        handler.removeCallbacksAndMessages(null)
+    }
+
 }
